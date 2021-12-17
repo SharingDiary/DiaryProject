6775290c32039ecd095b36b14f284046f6783b5f
kimyoojin <rladbwls027@naver.com>
2021-12-17 13:03:27 +0900

    커뮤니티 글 삭제 기능 구현

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var template = require('./lib/template.js');
+var community_template = require('./lib/community_template.js');
 var path = require('path');
 var qs = require('querystring');
 var mysql = require('mysql');
@@ -39,42 +40,87 @@ app.get('/group', function(req, res){
     res.send(html);
 });
 
-app.get('/community', function(req, res){
+app.get('/community', function(request, response) {
     var title = '커뮤니티';
-    var body = '<p>그룹원 모집글 목록 보기</p>';
+    let body = `<div class="search-group">
+        <p>그룹 검색</p>
+        </div>
+        <div class="group-container">`;
 
-    var html = template.HTML(template.loginNav(false), title, body);
-    res.send(html);
+    db.query(`SELECT * FROM recruitment_post`, function(error, posts) {
+        if (error) throw error;
+        db.query(`SELECT * FROM recruitment_post LEFT JOIN shary.group ON recruitment_post.group_id = group.group_id`, function(error2, groups) {
+            db.query(`SELECT * FROM recruitment_post_reply`, function(error2, replys) {
+                let i = 0;
+                // console.log(replys)
+                while(i<posts.length) {
+                    console.log(posts[i])
+                    let k = 0;
+                    let _id = posts[i].post_id;
+                    let writerId = posts[i].writer_id;
+                    let title = posts[i].title;
+                    let desc = posts[i].content;
+                    let people = groups[i].headcount;
+                    let reply = "";
+
+                    while(k<replys.length) {
+                        if(_id === replys[k].post_id) {
+                            reply += community_template.reply(replys[k].writer_id, replys[k].content);
+                        }
+                        k++;
+                    }
+                    
+                    // 현재 로그인중인 유저의 아이디로 변경
+                    if (writerId == "testuser"){
+                        body += community_template.RECRUITMENT_POST_WITH_DELETE_BUTTON(_id, title, people, "", desc, reply);
+                    }else{
+                        body += community_template.RECRUITMENT_POST(_id, title, people, "", desc, reply);
+                    }
+                    i++;
+                }
+                body += `</div>
+                <div class="group-reply-popup">
+                    <div class="group-reply-popup-wrap">
+                        <div class="group-question">
+                            <p><span class="group-invitaion-name">redvelvet</span> 님을 그룹으로 초대하시겠습니까?</p>
+                        </div>
+                        <div class="group-invitation-btn">
+                            <p>초대</p>
+                        </div>
+                    </div>
+                </div>`;
+
+                response.send(template.HTML(template.loginNav(false), title, body))
+            });
+        });
+    });      
 });
 
-app.get('/recruitment_post/:postId', function(req, res){
-    var title = '그룹원 모집글 조회';
-    var postId = parseInt(path.parse(req.params.postId).base);
+app.post('/delete_recruitment_post', function(req, res){
+    var body = '';
+    req.on('data', function(data){
+        body += data;
+    });
 
-    db.query(`SELECT * FROM recruitment_post WHERE post_id=${postId}`, 
-        function(error, topics){
+    req.on('end', function(){
+        var post = qs.parse(body);
+        var postId = parseInt(post.post_id);
+
+        // recruitment_post 테이블에 insert하기
+        db.query(`DELETE FROM recruitment_post WHERE post_id = ${postId}`, 
+            function(error, topics){
             if(error){
                 throw error;
             }
 
-            console.log("그룹원 모집글("+postId+") 조회 완료");
-            console.log(topics);
+            console.log("그룹원 모집글("+postId+")삭제 완료");
 
-            var body = `<h2 id="title">그룹원 모집글</h2>
-                    <div id="input_title">
-                        <h3>제목</h3>
-                        <input type="text" name="title" value="${topics[0].title}" readonly>
-                    </div>
-
-                    <div id="input_content">
-                        <h3>내용</h3>
-                        <textarea name="content" readonly>${topics[0].content}</textarea>
-                    </div>`;
-
-            var html = template.HTML(template.loginNav(false), title, body);
-            res.send(html);
+            res.writeHead(302, {Location: '/community'});
+            res.end();
         });
+    });
 });
+
 
 app.get('/create_group', function(req, res){
     var title = '그룹 생성';
@@ -123,12 +169,13 @@ app.post('/create_recruitment_post_process', function(req, res){
     req.on('end', function(){
         var post = qs.parse(body);
         var groupId = parseInt(post.groupId);
+        var writerId = "testuser" // 현재 로그인중인 유저의 아이디로 변경
         var title = post.title;
         var content = post.content;
 
         // recruitment_post 테이블에 insert하기
-        db.query(`INSERT INTO recruitment_post (group_id, title, content, reg_date) VALUES(?, ?, ?, NOW())`, 
-            [groupId, title, content], function(error, topics){
+        db.query(`INSERT INTO recruitment_post (group_id, writer_id, title, content, reg_date) VALUES(?, ?, ?, ?, NOW())`, 
+            [groupId, writerId, title, content], function(error, topics){
             if(error){
                 throw error;
             }
